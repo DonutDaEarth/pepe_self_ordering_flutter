b5efcce6b6dbd5103d53b6e1d06227495d4f3d30
DonutDaEarth <ss1.kurniawan@gmail.com>
2026-05-08 14:21:10 +0700

    feat: add registration, login, QR scanner, and main menu screens
    - Implemented RegisterScreen for user registration with form validation.
    - Added LoginScreen for user authentication with error handling.
    - Created QrScannerScreen to scan QR codes and navigate to the main menu.
    - Developed MainMenuScreen to display menu categories and items.
    - Included CartScreen for managing cart items and checkout process.
    - Added PaymentSuccessScreen and ReceiptScreen to handle order completion and display receipts.
    - Integrated state management using Provider for app and cart states.

--- lib/src/navigation/app_router.dart
+++ lib/src/navigation/app_router.dart
@@ -1,5 +1,5 @@
 import 'package:go_router/go_router.dart';
-import 'package:pepe_self_ordering_flutter/src/features/screens.dart';
+import 'package:pepe_self_ordering_flutter/src/screens/screens.dart';
 import 'package:pepe_self_ordering_flutter/src/state/app_state.dart';
 
 GoRouter buildRouter(AppState appState) {
@@ -11,7 +11,10 @@ GoRouter buildRouter(AppState appState) {
       GoRoute(path: '/qr_scanner', builder: (_, __) => const QrScannerScreen()),
       GoRoute(path: '/main_menu', builder: (_, __) => const MainMenuScreen()),
       GoRoute(path: '/cart', builder: (_, __) => const CartScreen()),
-      GoRoute(path: '/payment_success', builder: (_, __) => const PaymentSuccessScreen()),
+      GoRoute(
+        path: '/payment_success',
+        builder: (_, __) => const PaymentSuccessScreen(),
+      ),
       GoRoute(path: '/receipt', builder: (_, __) => const ReceiptScreen()),
     ],
   );
